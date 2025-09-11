b39e5925ca01c3265c39b0e9fd43d8d9a2c15aa4
rohan-18-ux <nnm24ee089@nmamit.in>
2025-09-11 12:28:37 +0530

    Update exp1a.c

--- exp1a.c
+++ exp1a.c
@@ -1,5 +1,5 @@
-//Name:Rakshith
-//USN:NNM24EE086
+//Name:Rohan
+//USN:NNM24EE089
 //Date:21/08/2025
 #include <reg51.h>
 unsigned char idata *R1=0x01, *internalmemory=0x44;
@@ -8,4 +8,5 @@ void main(void)
 	ACC=0x66;      
 	*R1=ACC;
 	ACC=*internalmemory;
+
 }
