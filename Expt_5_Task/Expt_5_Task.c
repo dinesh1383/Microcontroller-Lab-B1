281a08464743a4613a7fbc3fc91aa00b017607b2
nnm24ee092 <nnm24ee092@nmamit.in>
2025-10-16 12:27:33 +0530

    Add files via upload

---
+++ Expt_5_Task/Expt_5_Task.c
@@ -0,0 +1,23 @@
+// S Avaneeth
+// NNM24EE092
+// To display hex digits key value in both seven segment displays when the corresponding key in hex keypad is pressed
+#include <MicroLABlet.h> 
+#include <readkey.c> 
+sbit buzzer_control = P3^5; 
+unsigned char key_value=0, check ; 
+void main ( ) 
+{ 
+  buzzer_control=0; //output port
+  P2=0x0F; //higher nibble output port and lower nibble input port 
+  buzzer_control = 1; //disable buzzer 
+  while(1) 
+  {  
+    P2=0x0F; 
+    check=P2; 
+    if(check!=0x0F) 
+    { 
+      key_value= readkey(); 
+    } 
+    display(key_value);  
+  }    
+}
